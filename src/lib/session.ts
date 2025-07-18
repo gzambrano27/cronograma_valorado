5aea9f10803cb8ffafbdf1512e46da545f4f0c1d
Guillermo Alejandro Zambrano Moran <gzambrano@gpsgroup.com.ec>
2025-07-18 20:19:00 +0000

    no funciona, si quitas los cookies que puede pasar?

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,4 +1,3 @@
-
 'use server';
 import 'server-only';
 
@@ -6,20 +5,14 @@ import { getIronSession } from 'iron-session';
 import { cookies } from 'next/headers';
 import { sessionOptions, type SessionData } from './session-config';
 
-
-// This function gets a plain, serializable session data object.
-// It's safe to use in layouts and pages.
+// This function gets a plain, serializable session data object. Use this in layouts and pages.
 export async function getSession(): Promise<SessionData> {
-  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
-
-  // If the session object doesn't exist or isn't logged in, return a default state
-  if (!session || !session.isLoggedIn) {
-    return { isLoggedIn: false };
-  }
+  const cookieStore = cookies();
+  const session = await getIronSession<SessionData>(cookieStore, sessionOptions);
 
   // Return a plain object, not the session instance
   return {
-    isLoggedIn: session.isLoggedIn,
+    isLoggedIn: session.isLoggedIn || false,
     user: session.user,
     uid: session.uid,
     password: session.password,
