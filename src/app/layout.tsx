033ee14726a9b4bf308f60ffc7c34748c4377a71
Guillermo Alejandro Zambrano Moran <gzambrano@gpsgroup.com.ec>
2025-07-22 20:35:29 +0000

    Error: ./src/app/layout.tsx:36:6 Parsing ecmascript source code failed 

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
         
         {/* Metadatos y título de la aplicación */}
-        <meta name="theme-color" content="#3F51B5" />
+        <meta name="theme-color" content="#FFFFFF" />
          <title>Centro de Aplicaciones</title>
          <meta name="description" content="Una aplicación para valorar y gestionar el cronograma de proyectos." />
          <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><rect width='18' height='18' x='3' y='3' rx='2'/><path d='M7 7h3v3H7z'/><path d='M14 7h3v3h-3z'/><path d='M7 14h3v3H7z'/><path d='M14 14h3v3h-3z'/></svg>" />
